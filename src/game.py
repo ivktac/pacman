b31244febcc6b651b4e82183fc49abdcca4a6894
seshotake <seshotake@gmail.com>
2023-04-06 06:12:22 +0300

    add customize keyboard

--- src/game.py
+++ src/game.py
@@ -1,8 +1,11 @@
 import sys
+from functools import partial
+
 import pygame
-from settings import Settings
+
 from entities import Pacman
 from levels import Level
+from settings import Settings
 
 
 class Game:
@@ -11,16 +14,20 @@ class Game:
 
         self.settings = Settings()
         self.screen = pygame.display.set_mode(
-            [self.settings.screen["width"], self.settings.screen["height"]])
+            [self.settings.screen["width"], self.settings.screen["height"]]
+        )
         self.clock = pygame.time.Clock()
+
 
         self.level = Level(self)
         self.pacman = Pacman(self.level)
+        self.control_mapping = self.create_control_mapping()
+        
         self.load_level()
 
     def run(self) -> None:
         """
-            Запускає гру.
+        Запускає гру.
         """
 
         while True:
@@ -31,7 +38,7 @@ class Game:
 
     def handle_events(self) -> None:
         """
-            Обробляє події, такі як натискання клавіш і закриття вікна.
+        Обробляє події, такі як натискання клавіш і закриття вікна.
         """
 
         for event in pygame.event.get():
@@ -40,32 +47,48 @@ class Game:
                     pygame.quit()
                     sys.exit()
                 case pygame.KEYDOWN:
-                    match event.key:
-                        case pygame.K_ESCAPE:
-                            self.pause_game()
-                        case pygame.K_UP:
-                            self.pacman.change_direction(0, -1)
-                        case pygame.K_DOWN:
-                            self.pacman.change_direction(0, 1)
-                        case pygame.K_LEFT:
-                            self.pacman.change_direction(-1, 0)
-                        case pygame.K_RIGHT:
-                            self.pacman.change_direction(1, 0)
+                    self.process_keydown_event(event.key)
+
+    def process_keydown_event(self, key: int) -> None:
+        action = self.control_mapping.get(key)
+        if action is not None:
+            action()
+
+    def create_control_mapping(self) -> dict[int, callable]:
+        """
+        Створює словник, що містить відповідність між клавішами та діями.
+        """
+
+        controls = self.settings.controls
+        control_mapping = {
+            pygame.key.key_code(controls[direction]): partial(
+               self.pacman.change_direction, dx, dy
+            )
+            for direction, (dx, dy) in {
+                "up": (0, -1),
+                "down": (0, 1),
+                "left": (-1, 0),
+                "right": (1, 0),
+            }.items()
+        }
+        control_mapping[pygame.key.key_code(controls["pause"])] = self.pause_game
+
+        return control_mapping
 
     def update(self) -> None:
         """
-            Оновлює стан гри, викликавши метод оновлення об’єкта Level.
+        Оновлює стан гри, викликавши метод оновлення об’єкта Level.
 
-            Цей метод відповідає за оновлення позицій і станів усіх ігрових об’єктів.
+        Цей метод відповідає за оновлення позицій і станів усіх ігрових об’єктів.
         """
 
         self.level.update()
 
     def draw(self) -> None:
         """
-            Малює елементи гри на екрані, викликавши метод малювання об’єкта Level.
+        Малює елементи гри на екрані, викликавши метод малювання об’єкта Level.
 
-            Цей метод відповідає за відтворення всіх ігрових об’єктів на екрані.
+        Цей метод відповідає за відтворення всіх ігрових об’єктів на екрані.
         """
 
         self.clear_screen()
@@ -74,32 +97,32 @@ class Game:
 
     def clear_screen(self) -> None:
         """
-            Очищує екран коліром, визначеним в налаштуваннях перед кожним кадром.
+        Очищує екран коліром, визначеним в налаштуваннях перед кожним кадром.
         """
 
         self.screen.fill(self.settings.colors["background"])
 
     def show_menu(self) -> None:
         """
-            Відображає меню гри з такими опціями:
-                - `Нова гра` - починає гру з початку
-                - `Налаштування` - відкриває меню налаштувань
-                - `Вийти` - виходить з гри
+        Відображає меню гри з такими опціями:
+            - `Нова гра` - починає гру з початку
+            - `Налаштування` - відкриває меню налаштувань
+            - `Вийти` - виходить з гри
         """
         pass
 
     def pause_game(self) -> None:
-        """ 
-            Зупиняє гру та показує меню з такими опціями:
-                - `Продовжити` - продовжує гру
-                - `Перезапустити` - починає гру з початку
-                - `Вийти` - виходить з гри
+        """
+        Зупиняє гру та показує меню з такими опціями:
+            - `Продовжити` - продовжує гру
+            - `Перезапустити` - починає гру з початку
+            - `Вийти` - виходить з гри
         """
         pass
 
     def load_level(self, level_number=None) -> None:
         """
-            Завантажує дані рівня та створює відповідні об’єкти.
+        Завантажує дані рівня та створює відповідні об’єкти.
         """
 
         if level_number is None:
